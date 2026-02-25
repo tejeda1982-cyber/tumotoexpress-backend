0a8225afdf6e4982f25565f380b0eb9fa6e7b06d
Jonathan Tejeda <tejeda1982@gmail.com>
2026-02-24 23:43:37 -0300

    Actualiza precio en Cerebro.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function calcularPrecio(distancia_km) {
   } else {
     const km_6_a_10 = 4;
     const km_adicionales = distancia_km - 10;
-    neto = 6000 + (km_6_a_10 * 1000) + (km_adicionales * 900);
+    neto = 6000 + (km_6_a_10 * 1000) + (km_adicionales * 800);
   }
 
   const iva = Math.round(neto * 0.19);
