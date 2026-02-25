81eb8f25984e95d995291085ea7e30baad57931e
Jonathan Tejeda <tejeda1982@gmail.com>
2026-02-25 00:16:43 -0300

    Prueba nuclear

--- server.js
+++ server.js
@@ -54,9 +54,8 @@ function calcularPrecio(distancia_km) {
   } else {
   const km_6_a_10 = 4;
   const km_adicionales = distancia_km - 10;
-  neto = 6000 + (km_6_a_10 * 1000) + (km_adicionales * 800);
+  neto = 1;
 }
-
   const iva = Math.round(neto * 0.19);
   const total = neto + iva;
 
